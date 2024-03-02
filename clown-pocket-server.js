916b54dd84a098cc38637f8777d191cd5cccb99c
Rainyrou <janelyons211@gmail.com>
2024-03-02 21:07:18 +0800

    feat: Successfully deployed front-end and back-end

---
+++ clown-pocket-server.js
@@ -0,0 +1,6 @@
+const server = require('pushstate-server');
+
+server.start({
+  port: 5021,
+  directory: './dist'
+});
---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: 'clownPocket',
+      script: 'clown-pocket-server.js'
+    }
+  ],
+  deploy: {
+    production: {
+      user: 'root',
+      host: '114.132.167.29',
+      ref: 'origin/main',
+      repo: 'git@github.com:Rainyrou/Pocket-React.git',
+      path: '/workspace/clownPocket',
+      'post-deploy':
+        'git reset --hard && git checkout main && git pull && npm i --production=false && npm run build:release && pm2 startOrReload ecosystem.config.js',
+      env: {
+        NODE_ENV: 'production'
+      }
+    }
+  }
+};
