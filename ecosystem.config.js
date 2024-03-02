0a418512d24ed8d617999134a9973c6f49dd8221
Rainyrou <janelyons211@gmail.com>
2024-03-02 21:22:07 +0800

    feat: deployed front-end and back-end

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
       repo: 'git@github.com:Rainyrou/Pocket-React.git',
       path: '/workspace/clownPocket',
       'post-deploy':
-        'git reset --hard && git checkout main && git pull && npm i --production=false && pm2 startOrReload ecosystem.config.js',
+        'git reset --hard && git checkout main && git pull && npm i --production=false && npm run build:release && pm2 startOrReload ecosystem.config.js',
       env: {
         NODE_ENV: 'production'
       }
